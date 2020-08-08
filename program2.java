28c84278068f547a804e518d7ad4d68375cfbcec
sanyamjain9557 <49412309+sanyamjain9557@users.noreply.github.com>
2020-08-08 18:59:36 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,7 @@
+/*Write a program to get a substring of a StringBuffer.*/
+public class program2 {
+        public static void main(String [] args){
+    StringBuffer sf = new StringBuffer("I am the best influencer");
+    System.out.println(sf.substring(9,24));
+    
+}}
