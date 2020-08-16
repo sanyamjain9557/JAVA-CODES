721ca62c55239cb4f80a6435a0c15f15dae1a628
sanyamjain9557 <49412309+sanyamjain9557@users.noreply.github.com>
2020-08-16 18:04:29 +0530

    Write a Program that accepts two Strings as command line arguments and generate the output in the required format.

---
+++ ques3.java
@@ -0,0 +1,12 @@
+/*
+Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 
+
+Example1)
+If the two command line arguments are GLA and Mathura then the output generated should be GLA University Mathura.
+*/
+
+public class ques3{
+	public static void main(String args []){
+		System.out.println(args[0]+ " UNIVERSITY " +args[1]);
+	}
+}
