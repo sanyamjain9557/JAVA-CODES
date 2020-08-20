b41b6709452faecc39020be7b331e98d623fb268
sanyamjain9557 <49412309+sanyamjain9557@users.noreply.github.com>
2020-08-20 16:59:48 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class foreach{
+	public static void main(String args []){
+		
+		int ar[]={10,20,30,40,50,5,57,76,8,5};
+		int c=0;
+		for(int var:ar){
+			 c++;
+		}
+	System.out.println(c);
+}}
+
