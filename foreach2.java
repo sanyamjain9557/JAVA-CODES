65af6b8a4e993b3c265c0081eb8aae11e0252f3b
sanyamjain9557 <49412309+sanyamjain9557@users.noreply.github.com>
2020-08-20 17:10:55 +0530

    Add files via upload

---
+++ foreach2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class foreach2{
+	public static void main(String args []){
+		
+		String ar[]={"sa","ny","am","sanyam"};
+		
+		for(String var:ar){
+			 System.out.println(var);
+		}
+	
+}}
+
